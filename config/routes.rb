47e881856e9a4f91820b95e96c02d5f3e62a57fd
Saravanaselvan <spsaravanaselvan@gmail.com>
2015-07-30 21:40:38 +0530

    Removed gem controller and routes. Replaced with rack middleware

--- config/routes.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.routes.draw do
-  resource :get_data, :module => "ss2", :path => "getData"
-end
--- lib/rack/get_data.rb
+++ lib/rack/get_data.rb
@@ -1,19 +1,19 @@
 module Rack
-	class GetData
+  class GetData
     def initialize(app)
       @app=app
     end
 
     def call(env)
-    	if env['PATH_INFO'] == '/foo'
-    		request = Rack::Request.new(env)
-    		if request.params['jsonString'] != ""
-					error_code = Ss2.send_js_request request, request.params
-				end	
-    		[200, {"Content-Type" => 'text/plain'},["error_code: #{error_code}"]]
-    	else
-      	@app.call(env)
-    	end
+      if env['PATH_INFO'] == '/getData'
+        request = Rack::Request.new(env)
+        if request.params['jsonString'] != ""
+          error_code = Ss2.send_js_request request, request.params
+        end	
+        [200, {"Content-Type" => 'text/plain'},["error_code: #{error_code}"]]
+      else
+        @app.call(env)
+      end
     end		
-	end
+  end
 end
