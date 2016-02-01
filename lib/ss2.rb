d07ca5d96d9cfa136aa443f194131ceaaca43d8f
saravanaselvan <spsaravanaselvan@gmail.com>
2016-02-01 12:47:42 +0530

    monitor mode response updated

--- lib/ss2.rb
+++ lib/ss2.rb
@@ -159,9 +159,8 @@ module Ss2
 
 		else
 			syncresponse=shieldsquare_post_sync shieldsquare_service_url, shieldsquare_json_obj,@@timeout_value
-				$ShieldsquareResponse_responsecode = $ShieldsquareCodes_MONITOR
-				$ShieldsquareResponse_reason = syncresponse['output']
-			end
+			$ShieldsquareResponse_responsecode = $ShieldsquareCodes_MONITOR
+			$ShieldsquareResponse_reason = syncresponse['output']
 			$ShieldsquareResponse_dynamic_JS = "var __uzdbm_c = 2+2"
 		end
 
